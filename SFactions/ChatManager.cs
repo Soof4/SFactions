2bad723b7809c2e6d6cebc6dac7a87fbe7786562
Soof4 <emr3.cbc@gmail.com>
2024-06-20 20:53:44 +0300

    Change default chat format

--- SFactions/ChatManager.cs
+++ SFactions/ChatManager.cs
@@ -1,21 +1,25 @@
-﻿using TShockAPI;
+﻿using System.Reflection;
+using Org.BouncyCastle.Asn1.X509.Qualified;
+using TerrariaApi.Server;
+using TShockAPI;
 using TShockAPI.Configuration;
 using TShockAPI.Hooks;
 
-namespace SFactions {
-    public static class ChatManager {
-        
-        public static void OnPlayerChat(PlayerChatEventArgs args) {
-        
+namespace SFactions
+{
+    public static class ChatManager
+    {
+        public static void OnPlayerChat(PlayerChatEventArgs args)
+        {
             // Format the message
-            string message = string.Format(SFactions.Config.ChatFormat, 
+            string message = string.Format(SFactions.Config.ChatFormat,
                 "",    // {0}
                 args.Player.Group.Prefix,    // {1}
                 args.Player.Name,    // {2}
                 args.Player.Group.Suffix,    // {3}
                 args.RawText,    // {4}
                 SFactions.OnlineMembers.ContainsKey((byte)args.Player.Index) ? SFactions.OnlineFactions[SFactions.OnlineMembers[(byte)args.Player.Index]].Name : "");    // {5}
-                
+
             TSPlayer.All.SendMessage(message,
                 args.Player.Group.R,
                 args.Player.Group.G,
